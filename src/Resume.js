d80bab7c5068afd48bee396b908fd082d4f77885
devonmcdonald00 <37388480+devonmcdonald00@users.noreply.github.com>
2021-04-27 11:52:27 -0500

    Update Resume.js
    Resume.js change

--- src/Resume.js
+++ src/Resume.js
@@ -11,7 +11,7 @@ function Resume() {
     }, [])
     return (
         <div className='resume__Page'>
-            <iframe className='resume' title='resume' src="https://docs.google.com/document/d/e/2PACX-1vRy6pPKeT34B_4bWwpPUQASP3RZXN-NAteue1wUXy5ZLBzoDomHnGGHdIWJ7v3laQ/pub?embedded=true" onLoad={(e) => setLoading(false)} style={loading ? {display: 'none'} : {}}/>
+            <iframe className='resume' title='resume' seamless src="https://drive.google.com/file/d/11UgJnpUf-Ra8iCZaguTgBzhNBqbW36-a/preview" onLoad={(e) => setLoading(false)} style={loading ? {display: 'none'} : {}}/>
             {
                 loading ? 
                 <div style={{position: 'fixed', display: 'flex', flexDirection: 'column', right: "48%", top: "50%", justifyContent: 'center', alignItems: 'center', zIndex: 2000}}>
@@ -23,9 +23,9 @@ function Resume() {
             {
                 !loading &&
                 <div style={{display: 'flex', justifyContent: 'center', marginTop: 10, marginBottom: 60}}>
-                    <Link to='/Devon_McDonald_Resume(12-17-2020).docx' target='_blank' download className='resume__DownloadLink'>
+                    <a href={`${process.env.PUBLIC_URL}/Devon_McDonald_Resume_04_26_21.pdf`} target='_blank' download className='resume__DownloadLink'>
                         <button className='resume__DownloadButton'>Download Resume</button>
-                    </Link>
+                    </a>
                 </div>
             }
         </div>
